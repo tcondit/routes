610576f7d94f648216dafe09733d967fe1f692af
Tim Condit <tcondit@gmail.com>
2008-03-28 23:03:15 -0700

    Lots of additions, but neither of them are syntactically valid at the moment.

--- agents/graph.py
+++ agents/graph.py
@@ -8,15 +8,12 @@ class Graph(Area):
     def __init__(self):
         Area.__init__(self)
 
-    def getPoint(self):
-        '''Return a single (x,y) coordinate point'''
+    def get_location(self):
+        '''DOCSTRING'''
 	pass
 
-    def getDistance(self, here, there):
-        '''Return the distance between two points'''
-	pass
-
-    def updateLocation(self):
+    # set_location?  This and get_location are not very Pythonic.
+    def update_location(self):
         '''DOCSTRING'''
 	# Since Area is a separate class, I'm not sure how this method will
 	# work yet.  The idea is simple enough: an Agent is travelling from
@@ -25,5 +22,46 @@ class Graph(Area):
 	#
 	# Inputs include at least starting point and travel time; maybe also
 	# ending point and travelling speed.
+	pass
+
+    def get_point(self):
+        '''Return a single (x,y) coordinate point'''
+	pass
+
+    # Here's an example in sqlite3
+    #
+    # C:\Source\hg\unified\generated\data\AK\TGR02068\sql>sqlite3 TGR02068.db
+    # sqlite> SELECT id, frlong, frlat, tolong, tolat FROM tiger_01 ORDER BY random() LIMIT 1;
+    # 438|-149.987164|64.144821|-150.002351|64.131662
+    # sqlite> SELECT id, frlong, frlat, tolong, tolat FROM tiger_01 ORDER BY random() LIMIT 1;
+    # 2164|-148.808454|63.56553|-148.811602|63.564018
+    # sqlite>
+    #
+    # NOTE: this method is similar to agents.Agent mkcoords()
+    def getPoint(self):
+        '''Fetch a SQLAlchemy ResultProxy for a random point on the graph.'''
+        randomRow=random.randint(1,self.getRecordCount())
+	r=self.session.execute(select([
+		G.tiger01_Table.c.id,
+		G.tiger01_Table.c.tlid,
+		G.tiger01_Table.c.frlong,
+		G.tiger01_Table.c.frlat,
+		G.tiger01_Table.c.tolong,
+		G.tiger01_Table.c.tolat
+		],G.tiger01_Table.c.id==randomRow)).fetchone()
+#	for row in result:
+#            return row
+	# This thing returns a tuple with an int and four Decimal objects
+	# (which I don't know what the f--k to do with).
+	#
+	# Late note: maybe it's not so bad
+        # >>> print decimal.Decimal("-150.330257")
+        # -150.330257
+	# 
+        return r['id'],r['tlid'],r['frlong'],r['frlat'],r['tolong'],r['tolat']
+
+
+    def get_distance(self, here, there):
+        '''Return the distance between two points'''
 	pass
 
--- agents/grid.py
+++ agents/grid.py
@@ -8,22 +8,37 @@ class Grid(Area):
     def __init__(self):
         Area.__init__(self)
 
-    def getPoint(self):
+    def get_location(self):
+        '''DOCSTRING'''
+	pass
+
+    # set_location?  This and get_location are not very Pythonic.
+    def update_location(self):
+        '''DOCSTRING'''
+	# Since Area is a separate class, I'm not sure how this method will
+	# work yet.  The idea is simple enough: an Agent is travelling from
+	# one place to another, and for some reason needs to calculate their
+	# current location.
+	#
+	# Inputs include at least starting point and travel time; maybe also
+	# ending point and travelling speed.
+	pass
+
+    def get_point(self):
         '''Return a single (x,y) coordinate point'''
 	pass
 
-    def getDistance(self, here, there):
-        '''Return the distance between two points'''
+    def get_distance(dest, currentLocation=None):
+    (self, here, there):
+        '''
+Given a pair of coordinates, return the distance between them (float).
+
+Returns the straight-line distance between the points if hypotenuse is
+True (default).  Otherwise, returns the driving distance.
+        '''
 	pass
 
 def getdistance(dest, currentLocation=None):
-    '''
-Given a pair of coordinates, return the distance between them (float).
-
-Returns the straight-line distance between the points ("as the crow
-flies") if hypotenuse is True, (the default).  Otherwise, returns the
-driving distance.
-    '''
     # CAUTION: the compete methods do not use taxi_loc, so this is a hazard.
     curr = currentLocation or taxi_loc
     if not curr:
@@ -38,14 +53,34 @@ driving distance.
     elif DC == 'drivingDistance':
         return abs(curr[0]-dest[0]) + abs(curr[1]-dest[1])
 
+
     def updateLocation(self):
-        '''DOCSTRING'''
-	# Since Area is a separate class, I'm not sure how this method will
-	# work yet.  The idea is simple enough: an Agent is travelling from
-	# one place to another, and for some reason needs to calculate their
-	# current location.
-	#
-	# Inputs include at least starting point and travel time; maybe also
-	# ending point and travelling speed.
-	pass
+        '''
+Update the Taxi's current position.
+
+This method is normally only called from compete(), after a Taxi has been
+interrupted while en'route to a Fare.  The interruption means that another
+Taxi (the one doing the interrupting) got to the Fare first, and this Taxi
+needs to figure out where he is, so he can set his loc['curr'], and compete
+for the next Fare.
+
+Implementation detail: to keep things simple, I am just putting the Taxi near
+the halfway point between their former current location and their destination.
+
+NOTE: This method works under the assumption that the Taxi travels 1 unit of
+the grid for each tick of the simulation's clock.  This may eventually become
+a configuration setting, but it's low priority.
+        '''
+#        print '%s self.loc:' % self.name, self.loc
+        assert(self.loc['curr'])
+        assert(self.loc['dest'])
+
+        curr_tmp = {}
+        curr_tmp['x'] = ((self.loc['curr'][0] + self.loc['dest'][0])/2)
+        curr_tmp['y'] = ((self.loc['curr'][1] + self.loc['dest'][1])/2)
+
+        self.loc['curr'] = (curr_tmp['x'], curr_tmp['y'])
+        self.loc['dest'] = ()
+        curr_tmp.clear()
+        return
 
