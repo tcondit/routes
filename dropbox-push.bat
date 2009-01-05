a69dfd9cfbc13e153c572b105220679723756362
Tim Condit <tcondit@gmail.com>
2009-01-04 16:30:54 -0800

    Adding more stuff to save.

--- dropbox-push.bat
+++ dropbox-push.bat
@@ -1,7 +1,27 @@
-set REPO="C:\Source\hg\unified"
-set DROP="C:\Documents and Settings\Tim\My Documents\My Dropbox\unified"
+::
+:: Some of the folders in REPO_ROOT are Mercurial repositories, some of them
+:: are not.
+::
 
-cd %REPO%
-echo Pushing updates to Mercurial repository at %REPO%
+set REPO_ROOT="C:\Source\hg\unified"
+set DROP_ROOT="C:\Documents and Settings\Tim\My Documents\My Dropbox\hg"
+set CP="cp -priv --reply=yes"
+
+set REPO="envision"
+echo Pushing updates to Mercurial repository at %REPO_ROOT%\%REPO%
+cd %REPO_ROOT%\%REPO%
 hg push %DROP%
 echo done.
+
+set REPO="unified"
+echo Pushing updates to Mercurial repository at %REPO_ROOT%\%REPO%
+cd %REPO_ROOT%\%REPO%
+hg push %DROP%
+echo done.
+
+::
+:: This is rude and crude.
+::
+%CP% %REPO_ROOT%\django %DROP_ROOT%
+%CP% %REPO_ROOT%\tags %DROP_ROOT%
+%CP% %REPO_ROOT%\zipfiles %DROP_ROOT%
