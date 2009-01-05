37f9d374309ca1ef35d58032a4eef92be7e53845
Tim Condit <tcondit@gmail.com>
2009-01-04 16:32:09 -0800

    Fixing some bad ideas.

--- dropbox-push.bat
+++ dropbox-push.bat
@@ -3,25 +3,26 @@
 :: are not.
 ::
 
-set REPO_ROOT="C:\Source\hg\unified"
-set DROP_ROOT="C:\Documents and Settings\Tim\My Documents\My Dropbox\hg"
-set CP="cp -priv --reply=yes"
+set REPO_ROOT=C:\Source\hg
+set DROP_ROOT=C:\Documents and Settings\Tim\My Documents\My Dropbox\hg
 
-set REPO="envision"
+set REPO=envision
 echo Pushing updates to Mercurial repository at %REPO_ROOT%\%REPO%
-cd %REPO_ROOT%\%REPO%
-hg push %DROP%
+cd "%REPO_ROOT%\%REPO%"
+hg push "%DROP%"
 echo done.
 
-set REPO="unified"
+set REPO=unified
 echo Pushing updates to Mercurial repository at %REPO_ROOT%\%REPO%
-cd %REPO_ROOT%\%REPO%
-hg push %DROP%
+cd "%REPO_ROOT%\%REPO%"
+hg push "%DROP%"
 echo done.
 
 ::
 :: This is rude and crude.
 ::
-%CP% %REPO_ROOT%\django %DROP_ROOT%
-%CP% %REPO_ROOT%\tags %DROP_ROOT%
-%CP% %REPO_ROOT%\zipfiles %DROP_ROOT%
+::set CP=cp -priv
+::set CP=cp -priv --reply=yes
+::%CP% "%REPO_ROOT%\django" "%DROP_ROOT%"
+::%CP% "%REPO_ROOT%\tags" "%DROP_ROOT%"
+::%CP% "%REPO_ROOT%\zipfiles" "%DROP_ROOT%"
