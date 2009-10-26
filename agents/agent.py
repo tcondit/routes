d168e6615f3fe85cdd8bfc0acdc4d8df17f27d16
Tim <Tim@home-pc.hsd1.wa.comcast.net.>
2009-10-25 21:56:45 -0700

    Getting the agents files back up to what should be the latest. agents_driverdriver.py works fine, but graphs_driver.py is failing because of some problem extracting the FIPS ZIP files.  That does not appear to have anything to do with these changes.

--- agents/agent.py
+++ agents/agent.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
-'''DOCSTRING'''
+'''The Agent module, contains the Agent class'''
 
 import ConfigParser
 import os.path
 from grid import Grid
-# I may have to do something ugly to use this class from this location (ie:
-# accessing ../graphs/graph from agents/grid).
-#
-# Late note: or do the right thing, and put all these files into a single
-# directory.
-#from graphs.graph import Graph
+from graph import Graph
 
 config = ConfigParser.SafeConfigParser()
-config.read(os.path.join('agents', 'defaults.ini'))
-config.read(os.path.join('agents', 'overrides.ini'))
+config.read(os.path.join('agents','conf','agents','defaults.ini'))
+config.read(os.path.join('agents','conf','agents','overrides.ini'))
 
 # runtime config values
 STORECAP = config.getint('runtime', 'storeCapacity')
@@ -28,36 +23,39 @@ else:
     from SimPy.Simulation import *
 
 class Agent(Process):
-    '''DOCSTRING'''
+    '''
+    Agent is a SimPy Process.
 
+    Taxis and Fares are subclasses of the Agent class.
+    '''
     # Static (class) attributes!  Do NOT declare these in __init__ (or they
     # apparently won't be static).
     waiting=[]
-    waitingFares = Store(capacity=STORECAP, initialBuffered=waiting)
+    waitingFares=Store(capacity=STORECAP, initialBuffered=waiting)
     # I think this Monitor is used for ...
 #    waitingFares = Store(capacity=STORECAP, initialBuffered=waiting,
 #            monitored=True, monitorType=Monitor)
 
-    # This is a class attribute so that the SimPy filter functions
+    # These are class attributes so that the SimPy filter functions
     # closestfare_cooperate and mixedmode_cooperate in Taxi.py can use them
     if MAP_TYPE=='grid':
         map=Grid()
     elif MAP_TYPE=='graph':
         map=Graph()
 
+    print "MAP_TYPE:%s" % MAP_TYPE
+
     def __init__(self, name):
         Process.__init__(self, name)
-	if MAP_TYPE=='grid':
-            self.map=Grid()
-        elif MAP_TYPE=='graph':
-	    self.map=Graph()
-        self.loc = {}
-        self.loc['curr'] = self.map.get_location()
-        self.loc['dest'] = ()
+        self.loc={}
+        self.loc['curr']=Agent.map.get_location()[0]
+        self.loc['dest']=Agent.map.get_location()[1]
+
         self.ts = {}    # timestamps
         self.ts['activation'] = now()
+        print('%.4f\tactivated: [(Agent %s), (location %s)]' %
+            (self.ts['activation'], self.name, self.loc))
 
 if __name__ == '__main__':
-    a=Agent('Agent Smith')
+    a=Agent('Smith')
     print a.map.get_location()
-
